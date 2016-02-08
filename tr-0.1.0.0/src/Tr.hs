202f9c5a7f2dc946f5e7eb0d0741d7b337f5caf2
Johnson Tan <phisiart@gmail.com>
2016-02-08 10:12:08 +0800

    update Tr.hs

--- tr-0.1.0.0/src/Tr.hs
+++ tr-0.1.0.0/src/Tr.hs
@@ -12,6 +12,26 @@ module Tr
 -- | Just to give `tr` a more descriptive type
 type CharSet = String
 
+-- makeMap
+makeMap :: CharSet -> CharSet -> [(Char, Char)]
+makeMap [] (v : []) = []
+makeMap (k : ks) (v : []) = (k, v) : (makeMap ks (v : []))
+makeMap (k : ks) (v0 : v1 : vs) = (k, v0) : (makeMap ks (v1 : vs))
+
+-- replaceMode
+replaceMode :: [(Char, Char)] -> CharSet -> CharSet
+replaceMode lookupTable [] = []
+replaceMode lookupTable (k : ks) = (case lookup k lookupTable of
+                                      Just v -> v
+                                      Nothing -> k
+                                   ) : (replaceMode lookupTable ks)
+
+-- deleteMode
+deleteMode :: [Char] -> CharSet -> CharSet
+deleteMode dict [] = []
+deleteMode dict (k : ks) | elem k dict = deleteMode dict ks
+                         | otherwise = k : (deleteMode dict ks)
+
 -- | 'tr' - the characters in the first argument are translated into characters
 -- in the second argument, where first character in the first CharSet is mapped
 -- to the first character in the second CharSet. If the first CharSet is longer
@@ -31,5 +51,7 @@ type CharSet = String
 -- It's up to you how to handle the first argument being the empty string, or
 -- the second argument being `Just ""`, we will not be testing this edge case.
 tr :: CharSet -> Maybe CharSet -> String -> String
-tr _inset _outset xs = xs
+tr inset (Just outset) = replaceMode (makeMap inset outset)
+tr inset Nothing = deleteMode inset
+
 
